91b004e2a5c5f41ebf361216c79e5c068d06ad8e
andreij6 <andreij6@gmail.com>
2014-05-25 11:06:17 -0500

    initial

---
+++ PetApp.Data/Seeder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp.Data
+{
+    public static class Seeder
+    {
+        public static void Seed(
+            ApplicationDbContext context, 
+            bool seedShelter =  true, bool seedPets = true, bool seedVolunteers = true)
+        {
+            if (seedShelter)
+            {
+                Seeder.Shelters(context);
+            }
+
+            if(seedPets)
+            {
+                Seeder.Pets(context);
+            }
+
+            if(seedVolunteers)
+            {
+                Seeder.Volunteers(context);
+            }
+        }
+
+        private static void Shelters(ApplicationDbContext context)
+        {
+
+        }
+
+        private static void Pets(ApplicationDbContext context)
+        {
+
+        }
+
+        private static void Volunteers(ApplicationDbContext context)
+        {
+
+        }
+    }
+}
---
+++ PetApp.DataModels/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetApp.DataModels
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Adopted { get; set; }
+
+        public Pet()
+        {
+            this.Adopted = false;
+        }
+    }
+}
